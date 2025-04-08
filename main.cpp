ae310a19af8597e1073680c94a68e3ec31f768f4
bogdan nevmer <weckman689@gmail.com>
2025-04-08 18:47:31 +0300

    eqw

--- main.cpp
+++ main.cpp
@@ -2,27 +2,30 @@
 
 
 int mai() {
-	Student st1, st2;
-	st1.name = "veronika";
-	st1.age = 14;
-	st1.mark = 9.2;
-	st1.alive = true;
+	Student* st1=nullptr;
+	Student* st2 = nullptr;
+	st1 = new Student;
 
-	st2.name = "bogdan";
-	st2.age = 15;
-	st2.mark = 7;
-	st2.alive = true;
+	st1->name = "veronika";
+	st1->age = 14;
+	st1->mark = 9.2;
+	st1->alive = true;
+	   
+	st2->name = "bogdan";
+	st2->age = 15;
+	st2->mark = 7;
+	st2->alive = true;
 
 	cout << "before" << endl;
-	cout<<st1.getstring()<<endl;
-	cout<< st2.getstring() <<endl;
+	cout<<st1->getstring()<<endl;
+	cout<< st2->getstring() <<endl;
 	
 	st1 = st2;
-	st1.name = "bogdan";
+	st1->name = "bogdan";
 
 	cout << "after" << endl;
-	cout << st1.getstring() << endl;
-	cout << st2.getstring() << endl;
+	cout << st1->getstring() << endl;
+	cout << st2->getstring() << endl;
 
 	return 0;
 }
