3d4e1ebe8ed55e41ff59bd206f4acbc233629bb1
bogdan nevmer <weckman689@gmail.com>
2025-04-24 16:48:53 +0300

    update

--- main.cpp
+++ main.cpp
@@ -1,24 +1,25 @@
 #include "student.h"
 
 
-int mai() {
-
-	
-
-	Student  student1;
-	Student  student2("alex");
-	Student  student3("matvey",14);
-	Student  student4("bogdan",15,10,true);
+int main() {
 
 
+	Student student("Alex", 14, 5, true);
 
-	
-	cout << "after" << student1.tostring() << endl;
+	cout << student.toString() << endl;
 
-	cout << "after" << student2.tostring() << endl;
-	cout << "after" << student3.tostring() << endl;
+	for (int i = 0; i < student.countMarks; i++)
+	{
+		student.setMark(i, rand() % 7 + 4);
+	}
 
-	cout << "after" << student4.tostring() << endl;
+	cout << student.toString() << endl;
+
+	cout << "Student's marks: ";
+	for (int i = 0; i < student.countMarks; i++)
+	{
+		cout << student.getMark(i);
+	}
 
 	return 0;
 }
--- student.h
+++ student.h
@@ -2,49 +2,127 @@
 #include <string>
 using namespace std;
 
-
 class Student {
 public:
+
 	string name;
 	int age;
-	double mark;
+	int* marks;
+	int countMarks;
 	bool alive;
 
-	Student(string nm) {
-		cout << "defalt" << endl;
-		name = nm;
+	
+	Student() {
+		cout << "default-constructor..." << endl;
+		name = "no name";
 		age = 13;
-		mark = 4.0;
+		countMarks = 0;
+		marks = nullptr;
 		alive = true;
 	}
-
-	Student(string nm,int a,double m , double alv) {
-		cout << "defalt" << endl;
-		name = nm;
-		age = a < 13 ? 1 : a;
-		mark = 4.0;
-		alive = true;
-	}
-
-	Student(string nm, int a, double m, double alv) {
-		cout << "defalt" << endl;
-		name = nm;
-		age = student.age;
-		mark student.mark;
-		alive = true;
-	}
-
-
-	string tostring() {
-		string s = "name" + name;
-		s += ",age" + to_string(age);
-		s += ",mark " + to_string(mark);
-		s += ", alive " ;
-		s += alive ? "yes" : "no";
-		return s;
-
 
 	
+	Student(string nm) {
+		cout << "constructor with arguments (name)..." << endl;
+		name = nm;
+		age = 13;
+		countMarks = 0;
+		marks = nullptr;
+		alive = true;
+	}
 
+	
+	Student(string nm, int a) {
+		cout << "constructor with arguments (name, age)..." << endl;
+		name = nm;
+		age = a < 13 ? 13 : a;
+		countMarks = 0;
+		marks = nullptr;
+		alive = true;
+	}
+
+
+	Student(string nm, int a, int count, bool alv) {
+		cout << "canonical-constructor ..." << endl;
+		name = nm;
+		age = a;
+		countMarks = count;
+		marks = new int[count];
+
+		for (int i = 0; i < count; i++)
+		{
+			marks[i] = 4;
+		}
+
+		alive = alv;
+	}
+
+	Student(const Student& student) {
+		cout << "copy-constructor ..." << endl;
+		name = student.name;
+		age = student.age;
+		countMarks = student.countMarks;
+		marks = new int[countMarks];
+
+		for (int i = 0; i < countMarks; i++)
+		{
+			marks[i] = student.marks[i];
+		}
+
+		alive = student.alive;
+	}
+
+
+	~Student() {
+		cout << "destructor..." << endl;
+
+		if (countMarks > 0) {
+			delete[] marks;
+		}
+	}
+
+
+	string toString() {
+		string s = "Name: " + name;
+		s += ", age: " + to_string(age);
+		s += ", marks " + convert();
+		s += ", alive: ";
+		s += alive ? "yes" : "no";
+		return s;
+	}
+
+	string convert() {
+		string s = "[";
+
+		if (countMarks > 0) {
+			for (int i = 0; i < countMarks - 1; i++)
+			{
+				s += to_string(marks[i]) + ", ";
+			}
+
+			s += to_string(marks[countMarks - 1]);
+		}
+
+		s += "]";
+
+		return s;
+	}
+
+	int getMark(int index) {
+		if (countMarks == 0 || index < 0
+			|| index >= countMarks) {
+			return -1;
+		}
+
+		return marks[index];
+	}
+
+	void setMark(int index, int mark) {
+		if (countMarks == 0 || index < 0 || index >= countMarks
+			|| mark < 0 || mark > 10) {
+			return;
+		}
+
+		marks[index] = mark;
 	}
 };
